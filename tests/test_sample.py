0d8caad2f3bef1bc71e2d38990cc4810c9f3d8c3
Louis Auger <jammyninja95@gmail.com>
2025-12-03 09:14:41 +0000

    project skeleton setup

---
+++ tests/test_sample.py
@@ -0,0 +1,11 @@
+# pylint: disable-all
+
+import unittest
+
+class TestSample(unittest.TestCase):
+    def test_sample(self):
+        # We are simply checking whether 42==42!
+        self.assertEqual(42, 42) #raises an error if false
+
+        # your_result = your_function()
+        # self.assertEqual(your_result, correct_answer)
